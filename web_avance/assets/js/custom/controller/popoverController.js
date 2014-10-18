7e388d5de9673815cf5887f483d5669005a60146
JIN Benli <benli0822@gmail.com>
2014-10-18 13:57:51 +0200

    popover controller
    all popover control method will be defined here

--- web_avance/assets/js/custom/controller/popoverController.js
+++ web_avance/assets/js/custom/controller/popoverController.js
@@ -2,5 +2,15 @@
  * Created by benli on 16/10/14.
  */
 popover = {
+    updatePopoverConetent: function (divid) {
+        if(typeof(tdb) == "undefined") {
+            console.log("Teacher database is not loaded, please make sure it is loaded!");
+            return;
+        }
 
+        var id_colume = parseInt(divid.substring(9, 10));
+        var id_row = parseInt(divid.substring(10, 11));
+
+        var theteacher = tdb.teacherList[id_colume];
+    }
 }
