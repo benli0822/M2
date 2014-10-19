1256d07fd9885e13e7c667d59c6c279300f7fc2e
JIN Benli <benli0822@gmail.com>
2014-10-19 18:29:34 +0200

    finish debugging get user selected value with webui popover, seems this plugin will generate each time a new popover div, so I have used jquery with selecting the last explicit node

--- web_avance/assets/js/custom/controller/buttonActionController.js
+++ web_avance/assets/js/custom/controller/buttonActionController.js
@@ -2,14 +2,30 @@
  * Created by CHENG Xiaojun et JIN Benli on 08/10/14.
  */
 
-function search_button_click(){
-    var search_text = document.getElementById("searchTextField").value;
+buttonAction = {
+    search_button_click: function () {
+        var search_text = document.getElementById("searchTextField").value;
 
-    console.log("you want to search : " + search_text);
+        console.log("you want to search : " + search_text);
 
         document.getElementById("searchResult").innerHTML = 'clicked';
         var the_student = sdb.find_a_client_by_firstname(search_text);
-    if(typeof(the_student) != 'undefined') {
-        table.update_student_table(the_student);
+        if (typeof(the_student) != 'undefined') {
+            table.update_student_table(the_student);
+        }
+    },
+
+    create_event_click: function () {
+//    var time = document.getElementById('pop_time').getAttribute('value').value;
+        var time = $('.webui-popover-content:last .form-horizontal #when div #pop_time').attr('value');
+        var teacher = $('.webui-popover-content:last .form-horizontal #teacher div #pop_teacher').text();
+        var student = $('.webui-popover-content:last .form-horizontal #student div #pop_student').val();
+        var selectedClass = $('.webui-popover-content:last .form-horizontal #classes div input[name=optionClass]:checked').val();
+
+        console.log('Selected time: ' + time);
+        console.log('Selected teacher: ' + teacher);
+        console.log('Selected student: ' + student);
+        console.log('Selected selectedClass: ' + selectedClass);
+
     }
 }
