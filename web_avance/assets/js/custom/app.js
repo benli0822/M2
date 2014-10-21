db7ea6444c109d127bd04c092d2edb743a2528b4
jamescheng16 <jamescheng16@gmail.com>
2014-10-21 10:24:38 +0200

    登录的时候的学生表格更新

--- web_avance/assets/js/custom/app.js
+++ web_avance/assets/js/custom/app.js
@@ -30,7 +30,7 @@ window.onload = function () {
     table.addTimeLine('student_table', 4);
     table.createSecretaryTable('secretary_table');
     table.updateSecretaryTableContent(new Date());
-    table.update_student_table(sdb.find_a_client_by_name('stu1', 'stu1'));
+    //table.update_student_table(sdb.find_a_client_by_name('stu1', 'stu1'));
     //table.updateSecretaryTableContent();
 //    popover.updateTableContent();
 
--- web_avance/assets/js/custom/controller/loginController.js
+++ web_avance/assets/js/custom/controller/loginController.js
@@ -12,11 +12,18 @@ loginController = {
         if (sedb.login_secretary(name[0],name[1],password)){
             console.log("find the secretary" + username);
 
+
             $("#loginModule").fadeOut();
             $("#secretaryModule").fadeIn();
         }
         else if (sdb.student_login(name[0],name[1],password)){
             console.log("find the stu" + username);
+
+
+            //update student table
+            table.update_student_table(sdb.find_a_client_by_name(name[0],name[1]));
+
+
             $("#loginModule").fadeOut();
 
             $("#studentModule").fadeIn();
