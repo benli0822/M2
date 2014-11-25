54c3201e3ada60a871497f7e19a0a65a2271de69
JIN Benli <benli0822@gmail.com>
2014-11-25 16:30:26 +0100

    create test for localStorageWrapper.js

---
+++ web_avance/test/qunit/model/database/localStorageWrapper_test.js
@@ -0,0 +1,18 @@
+/**
+ * Created by benli on 25/11/14.
+ */
+module("localStorageWrapper", {
+//	setup:function(){alert("setup class individual test");},
+//	teardown:function(){alert("teardown class individual test");}
+});
+
+test("test for wrapper function", function () {
+        var lswrapper = new LocalStorageWrapper();
+
+        var student = new Client("bob","bob","Lille","bob");
+
+        var key = lswrapper.getPersonKey(student);
+
+        equal(key, "bobbobLille", "Created key for test student is: bobbobLille");
+    }
+);
