05692348317ca30a3445be5282c7383494019ef4
JIN Benli <benli0822@gmail.com>
2014-12-03 10:43:31 +0100

    create teacher db test

---
+++ web_avance/test/qunit/model/database/teacherDB_test.js
@@ -0,0 +1,22 @@
+/**
+ * Created by benli on 03/12/14.
+ */
+module("teacherDB", {
+//	setup:function(){alert("setup class individual test");},
+//	teardown:function(){alert("teardown class individual test");}
+});
+
+test("test for teacher initialisation for the first time", function () {
+        localStorage.clear();
+
+        var tdb = new TeacherDB();
+
+        // here we use a deepEqual for a recursive comparison, especially for an empty array
+        deepEqual(tdb.teacherList, [], "First tiem teacher database store an empty list");
+    }
+);
+
+test("test for teacher initialisation with reloading local storage's content", function () {
+
+    }
+);
